59b6b532ab7242190c7778118e7b49ca429c3c32
Georgii Solovev <georgy_s@netmonet.co>
2026-01-03 20:08:50 +0300

    review

--- src/Converter.java
+++ src/Converter.java
@@ -1,10 +1,14 @@
 public class Converter {
-    int convertToKm(int steps) {
-        int km = (steps * 75) / 100_000;
-        return km;
-    }
-    int convertStepsToKilocalories(int steps) {
-        int kilocalories = (steps * 50) / 1000;
-        return kilocalories;
-    }
+
+  private Converter() {
+
+  }
+
+  public static int convertToKm(int steps) {
+    return (steps * 75) / 100_000;
+  }
+
+  public static int convertStepsToKilocalories(int steps) {
+    return (steps * 50) / 1000;
+  }
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,35 +1,31 @@
 import java.util.Scanner;
 
 public class StepTracker {
-    Scanner scanner;
-    MonthData[] monthToData = new MonthData[12];
-    int goalByStepsPerDay = 10000;
-    Converter converter = new Converter();
 
+    private final Scanner scanner;
+    private final MonthData[] monthToData = new MonthData[12];
+    private int goalByStepsPerDay = 10000;
 
     StepTracker(Scanner scan) {
-        scanner = scan;
+        this.scanner = scan;
 
         for (int i = 0; i < monthToData.length; i++) {
             monthToData[i] = new MonthData();
         }
     }
-    void addNewNumberStepsPerDay(int month, int day, int steps) {
-        if (month < 1) {
-            System.out.println("Invalid month");
-            return;
-        } else if (month > 12) {
-            System.out.println("Invalid month");
-            return;
+
+    public void addNewNumberStepsPerDay(int month, int day, int steps) {
+        if(!isMonthValid(month)) {
+          System.out.println("Invalid month");
+          return;
         }
-        if (day < 1) {
-            System.out.println("Invalid day");
-            return;
-        } else if (day > 30) {
-            System.out.println("Invalid day");
-            return;
+
+        if(!isDayValid(day)) {
+          System.out.println("Invalid day");
+          return;
         }
-        if (steps < 1) {
+
+        if (!isStepsValid(steps)) {
             System.out.println("Invalid steps");
             return;
         }
@@ -37,19 +33,18 @@ public class StepTracker {
         MonthData monthData = monthToData[month-1];
         monthData.days[day-1] = steps;
         System.out.println(monthData.days[day-1] + " шагов за " + month + " месяц, " + day + " день - cохранено" );
-
     }
 
-    void changeStepGoal(int steps) {
+  public void changeStepGoal(int steps) {
         if (steps <= 0) {
             System.out.println("Нам нужны цели выше!");
             return;
         }
         goalByStepsPerDay = steps;
-        System.out.println("Цель успешно изменена на новую: " + goalByStepsPerDay + " шагов");
+        System.out.println("Цель успешно изменена на новую: %s шагов".formatted(goalByStepsPerDay));
     }
 
-    void printStatistic() {
+  public void printStatistic() {
         System.out.println("Введите число месяца");
         int month = scanner.nextInt();
         MonthData monthData = monthToData[month-1];
@@ -58,9 +53,21 @@ public class StepTracker {
         System.out.println("Общее количество шагов за месяц: " + sumSteps);
         System.out.println("Максимальное пройденное количество шагов в месяце: " + monthData.maxSteps());
         System.out.println("Cреднее количество шагов за месяц: " + sumSteps / 30);
-        System.out.println("Пройденная дистанция (в км): " + converter.convertToKm(sumSteps));
-        System.out.println("Количество сожжённых килокалорий: " + converter.convertStepsToKilocalories(sumSteps));
+        System.out.println("Пройденная дистанция (в км): " + Converter.convertToKm(sumSteps));
+        System.out.println("Количество сожжённых килокалорий: " + Converter.convertStepsToKilocalories(sumSteps));
         System.out.println("Лушчая серия: " + monthData.bestSeries(goalByStepsPerDay));
+    }
+
+    private boolean isMonthValid(int month) {
+      return month > 0 && month <= 12;
+    }
+
+    private boolean isDayValid(int day) {
+      return day > 0 && day <= 30;
+    }
+
+    private boolean isStepsValid(int steps) {
+      return steps > 0;
     }
 
 }
